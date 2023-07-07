3137e0338b01214b20f2e81733fdcdfe8df9ded6
Rume7 <electengrhume@gmail.com>
2023-07-07 23:52:39 +0100

    Added multiplication entity.

--- src/main/java/com/codehacks/sm/domain/Multiplication.java
+++ src/main/java/com/codehacks/sm/domain/Multiplication.java
@@ -1,5 +1,31 @@
-package com.codehacks.sm.model;
+package com.codehacks.sm.domain;
 
 public class Multiplication {
 
+    private int factorA;
+    private int factorB;
+    private int result;
+
+    public Multiplication(int factorA, int factorB) {
+        this.factorA = factorA;
+        this.factorB = factorB;
+    }
+
+    public int getFactorA() {
+        return factorA;
+    }
+
+    public int getFactorB() {
+        return factorB;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Multiplication{ factorA = " + factorA +
+                ", factorB = " + factorB + ", result(A*B)=" + result + '}';
+    }
 }
