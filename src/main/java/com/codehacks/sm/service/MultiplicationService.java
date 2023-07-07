dc27c53b150897e47e9d9f9bec15ccab27f04b69
Rume7 <electengrhume@gmail.com>
2023-07-08 00:03:41 +0100

    Added interfaces for the service class.

--- src/main/java/com/codehacks/sm/service/MultiplicationService.java
+++ src/main/java/com/codehacks/sm/service/MultiplicationService.java
@@ -1,2 +1,17 @@
-package com.codehacks.sm.service;public class MultiplicationService {
+package com.codehacks.sm.service;
+
+import com.codehacks.sm.domain.Multiplication;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface MultiplicationService {
+
+    /**
+     * Create a Multiplication object with two randomly-generated
+     * factors between 11 and 99.
+     *
+     * @return a Multiplication object with random objects.
+     */
+    Multiplication createRandomMultiplication();
+
 }
